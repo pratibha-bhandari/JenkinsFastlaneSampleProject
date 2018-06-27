03ea152bee35813765f63adce7b136c8e0b3efbf
Pratibha <pratibha8288@gmail.com>
2018-06-27 11:49:53 +0530

    Added msg text

--- JenkinsFastlaneSampleProject/ViewController.swift
+++ JenkinsFastlaneSampleProject/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let messageText = "Hello Jenkins"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("message - \(messageText)")
         
     }
 
