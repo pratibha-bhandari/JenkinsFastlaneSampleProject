52b4e4644ae23429d009daa2ce99bd7589834db5
Pratibha <pratibha8288@gmail.com>
2018-06-28 09:51:45 +0530

    msg edited

--- JenkinsFastlaneSampleProject/ViewController.swift
+++ JenkinsFastlaneSampleProject/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var msgLabel: UILabel!
     
-    let messageText = "Hello Jenkins"
+    let messageText = "Welcome to Jenkins"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
